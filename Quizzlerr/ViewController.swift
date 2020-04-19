50e29e7d0b7728b96f434395eaa9238ce1269c45
Nabeel <tmnabeel30@gmail.com>
2020-04-20 03:56:54 +0530

    Initial Commit

---
+++ Quizzlerr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizzlerr
+//
+//  Created by Nabeel on 4/20/20.
+//  Copyright © 2020 TMN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
